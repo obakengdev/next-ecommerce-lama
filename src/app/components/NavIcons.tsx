59adc3693fa17cd4c7664c4d96dde260ab6226d3
obakengdev <obakengdev@gmail.com>
2025-02-05 16:48:05 +0200

    Updated nav icons with images and profile handling

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -1,3 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export const NavIcons = () => {
-  return <div>NavIcons</div>;
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const router = useRouter();
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen((prev) => !prev);
+  };
+  return (
+    <div className="flex items-center gap-4 xl:6">
+      <Image
+        src="/profile.png"
+        alt=""
+        width="22"
+        height="22"
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb{0,0,0,0.2}] z-20">
+          <Link href="/">Profile</Link>
+          <div className="mt-2 cursor-pointer">Logout</div>
+        </div>
+      )}
+      <Image
+        src="/notification.png"
+        alt=""
+        width="22"
+        height="22"
+        className="cursor-pointer"
+      />
+      <Image
+        src="/cart.png"
+        alt=""
+        width="22"
+        height="22"
+        className="cursor-pointer"
+        onClick={() => setIsCartOpen((prev) => !prev)}
+      />
+    </div>
+  );
 };
