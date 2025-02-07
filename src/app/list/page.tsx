62f7f4685a1788265706c4ac5521e773f752b3f7
obakengdev <obakengdev@gmail.com>
2025-02-07 15:04:01 +0200

    Added Filter component and updated list page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
+import { Filter } from "../components/Filter";
+import { ProductList } from "../components/ProductList";
 
 export const ListPage = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
-      <div className="bg-pink-50 px-4 flex justify-between h-64">
+      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
         <div className="w-2/3 flex flex-col items-center justify-center gap-8">
           <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
-            Grab up to 50% off on Selected Products
+            Grab up to 50% off on
+            <br /> Selected Products
           </h1>
           <button className="rounded-3xl bg-custom text-white w-max py-3 px-5 text-sm">
             Buy Now
           </button>
         </div>
-        <div className="w-1/3">
+        <div className="relative w-1/3">
           <Image src="/woman.png" alt="" fill className="object-contain" />
         </div>
       </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You</h1>
+      <ProductList />
     </div>
   );
 };
